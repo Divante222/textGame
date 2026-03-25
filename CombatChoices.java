32cd1f8c010154578987101f7f1fc6e04d54bf05
divante222 <divante111@gmail.com>
2026-03-24 21:59:57 -0500

    view stats added

--- CombatChoices.java
+++ CombatChoices.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class CombatChoices{
     Scanner sc = new Scanner(System.in);
-    List<String> actions = new ArrayList(Arrays.asList("Attack", "Defend", "Use Ability"));
+    List<String> actions = new ArrayList(Arrays.asList("Attack", "Defend", "Use Ability", "Menu"));
     Random randomNumber = new Random();
     
     public List<Unit> getPlayerCombatChoices(List<Unit> monsterEncountered, Unit character, Campaign campaign){
@@ -38,6 +38,50 @@ public class CombatChoices{
             } else if(playerCombatChoice.equals("3")){
                 System.out.println("Player is using Ability");
                 character.abilities.get(0).AbilityExecution();
+                break;
+            } else if(playerCombatChoice.equals("4")){
+                
+                String menuSelection = "";
+                while(menuSelection != "Exit"){
+                    System.out.println("Menu");
+                    System.out.println("=======================================================\n");
+                    System.out.println("Inventory::1\n");
+                    System.out.println("Stats::2\n");
+                    System.out.println("Equipment::3\n");
+                    System.out.println("Exit::4\n");
+                    System.out.println("=======================================================\n");
+                    menuSelection = sc.nextLine();
+                    switch(menuSelection) {
+                        case "1":
+                            System.out.println("Inventory is a work in progress");
+                            break;
+                        case "2":
+                            System.out.println("Stats");
+                            System.out.println("=======================================================\n");
+                            System.out.println("Strength:: " + character.getStrength());
+                            System.out.println("Dexterit:: " + character.getDexterity());
+                            System.out.println("Constitution:: " + character.getConstitution());
+                            System.out.println("Willpower:: " + character.getWillpower());
+                            System.out.println("Intelligence:: " + character.getIntelligence());
+                            System.out.println("Wisdom:: " + character.getWisdom());
+                            System.out.println("\n=======================================================\n");
+                            System.out.println("Press enter to continue...");
+                            sc.nextLine();
+                            break;
+                        case "3":
+                            System.out.println("Equipment");
+                            break;
+                        case "4":
+                            menuSelection = "Exit";
+                            break;
+                        default:
+                            System.out.println("\n-------------------------------------------------------\n");
+                            System.out.println("Please enter a valid selection!");
+                            System.out.println("\n-------------------------------------------------------\n");
+                            System.out.println("Press Enter to continue...");
+                            sc.nextLine();
+                    }
+                }
                 break;
             } else{
                 System.out.println("Please enter a valid selection");
@@ -148,12 +192,12 @@ public class CombatChoices{
     public String playerCombatText(){
         int iteration = 1;
         System.out.println("Make your move");
-        System.out.println("=======================================================");
+        System.out.println("==========================================================================================");
         for(Object action : this.actions){
-            System.out.print(action.toString() + ": " + iteration + "\t");
+            System.out.print(action.toString() + ": " + iteration + "\t\t");
             iteration +=1;
         }
-        System.out.println("\n=======================================================\n");
+        System.out.println("\n==========================================================================================\n");
         System.out.print("Selection: ");
         String playerCombatChoice = sc.nextLine();
         System.out.println();
